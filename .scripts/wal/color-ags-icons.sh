3e0a5d1e79024b0268f18973d4297c6432f125ca
mgiks <lidamil6785@gmail.com>
2025-05-26 12:54:04 +0600

    stuff

--- .scripts/wal/color-ags-icons.sh
+++ .scripts/wal/color-ags-icons.sh
@@ -4,5 +4,5 @@ ags_icons_files=($(fd . "$XDG_CONFIG_HOME/ags/icons"))
 
 for ags_icon_file in ${ags_icons_files[@]}; do
     new_svg=$(cat $ags_icon_file | tr " " '\n' | sed "s/fill=\".\+\"/fill=\""$color1"\"/" | tr '\n' " ")
-    echo "$new_svg" >$ags_icon_file
+    echo -n "$new_svg" >$ags_icon_file
 done
