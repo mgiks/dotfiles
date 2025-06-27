e82caecd51d47cc047e9c04960a2daba70dac837
mgiks <lidamil6785@gmail.com>
2025-06-27 11:20:46 +0600

    fix: update icons correctly

--- dot_scripts/wal/executable_color-ags.sh
+++ dot_scripts/wal/executable_color-ags.sh
@@ -3,12 +3,10 @@ cat "$HOME/.cache/wal/colors.scss" >"$HOME/.config/ags/utility-styles/_generated
 . "/home/mgik/.cache/wal/colors.sh"
 
 ags_icon_folder="/home/mgik/.config/ags/icons/"
-ags_icons_files=$(ls "$ags_icon_folder")
+ags_icon_files=("$ags_icon_folder"/*.svg)
 
-for ags_icon_file in "${ags_icons_files[@]}"; do
-    new_svg="$(tr " " '\n' <"$ags_icon_file" | sed "s:fill=\".\+\":fill=\"$color1\":" | tr '\n' " ")"
-
-    echo -n "$new_svg" >"$ags_icon_file"
+for ags_icon_file in "${ags_icon_files[@]}"; do
+    sed "s|\(fill=\)\(.\).\{7\}\(.\)|\1\2$color1\3|" -i "$ags_icon_file"
 done
 
 ags quit &>/dev/null
