b740914d0939ef771acc78ca97be56b40e867acd
mgiks <lidamil6785@gmail.com>
2026-05-28 18:38:35 +0600

    Update .scripts/pacman/gist.sh

--- dot_scripts/pacman/executable_gist.sh
+++ dot_scripts/pacman/executable_gist.sh
@@ -1,10 +1,16 @@
 #! /usr/bin/env bash
+set -euo pipefail
+
 source "$(dirname "$0")"/.gh_token
 
 if [ -z "${GH_TOKEN:-}" ]; then
     echo .gh_token file is missing
     exit 1
 fi
+
+get_package_names() {
+    pacman -Qte | awk '{print $1}'
+}
 
 echo Checking whether pacman-packages gist exists...
 pacman_packages_gist=$(gh gist list | grep pacman-packages | head -n1 | awk '{ print $1 }')
@@ -13,7 +19,7 @@ file='pacman-packages'
 if [ -z "${pacman_packages_gist:-}" ]; then
     touch $file
 
-    pacman -Qe | awk '{ print $1 }' >$file
+    get_package_names >$file
     echo Creating pacman-packages gist...
     gh gist create $file >/dev/null
 
@@ -23,4 +29,4 @@ fi
 gist_id=$(gh gist list | grep $file | head -n1 | awk '{print $1}')
 
 echo Updating pacman-packages gist...
-gh gist edit "$gist_id" <(pacman -Qe | awk '{print $1}')
+gh gist edit "$gist_id" <(get_package_names)
